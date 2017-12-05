dd81c9655128aa7d9c32a8b292d66f007c0e81ee
Andrius Urbelis <andrius.urbelis@gmail.com>
2017-12-05 22:59:36 +0200

    searc t line

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace ReadFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            var fs = new FileStream("C:\\temp\\32_20171205001\\32_20171205001.TSYSO", FileMode.Open);
+
+            using(var r = new StreamReader(fs))
+            {
+                var line = " ";
+                var i = 0;
+
+                while((line = r.ReadLine()) != null)
+                {
+                    i++;
+                    Console.WriteLine(i);
+
+                    if (line.StartsWith("T"))
+                    {
+                        Console.WriteLine(i);
+                        Console.WriteLine(line); 
+                    }
+                }
+            }
+
+            stopWatch.Stop();
+            Console.WriteLine(stopWatch.Elapsed);            
+        }
+    }
+}
